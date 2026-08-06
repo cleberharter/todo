b885ca3970834e166c88fcb86cd1679cbe40685b
cleberharter <cleberharter@gmail.com>
2026-08-06 08:16:05 -0300

    atualiza título da aplicação para "Minhas Tarefas 15" e ajusta mapeamento de branches para ambientes no CI/CD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,7 +51,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Minhas Tarefas 14</h1>
+      <h1>Minhas Tarefas 15</h1>
 
       <form className="todo-form" onSubmit={handleSubmit}>
         <input
